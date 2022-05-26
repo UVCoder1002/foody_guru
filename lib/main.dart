32386c05cf1320edffc56361011f870537a22996
Urvashi Gupta <uv6coder@gmail.com>
2022-05-26 17:27:56 +0530

    Reset Password Validation and Animation Added,WelcomePage UI Edited

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 // import 'dart:html';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import 'welcomePage.dart';
+import 'welcomepage.dart';
+import 'dart:math' as Math;
 
 void main() => runApp(MyApp());
 class MyApp extends StatelessWidget{
@@ -22,7 +24,7 @@ class MyApp extends StatelessWidget{
 class LoginPage extends StatefulWidget{
   _LoginPageState createState() => _LoginPageState();
 }
-class _LoginPageState extends State<LoginPage>{
+class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMixin{ 
 //Controllers for  :- Login
 final email_controller = TextEditingController();
 final password_controller= TextEditingController();
@@ -37,14 +39,60 @@ late bool isLoggedIn = false;
 //For hiding forgot password link
 late bool isVisibile = true;
 final _formKey = GlobalKey<FormState>();
+final _resetKey = GlobalKey<FormState>();
+
 String username = '';
+
+int val=0;
+
+  late Image cardFront;
+  late Image cardBack;
+  bool showFront = true;
+  late AnimationController controller;
+  double anglePlus=0;
 
 void initState(){
   super.initState();
+  // email_controller.addListener(validate_email());
+  // password_controller.addListener();
+  // email_controller2.addListener();
+  // newPassword_controller.addListener();
+  // re_newPassword_controller.addListener();
    autoLogin();
+    cardFront = Image.asset("Images/foody_guru_logo.png");
+    cardBack  = Image.asset("Images/fodd_time.png");
+    WidgetsBinding.instance!.addPostFrameCallback((_) {precacheImage(cardFront.image, context);precacheImage(cardBack.image, context); 
+    });
+    // Initialize the animation controller
+    controller = AnimationController(vsync: this, duration: Duration(milliseconds: 4000), value: 0);
+     controller.forward();
+    controller.addListener(() {
+      if(controller.isCompleted||controller.isDismissed){
+        flipImage();
+      }
+    
+    });
 }
 
-
+// String? get _errorText_newpassword {
+//   String value = newPassword_controller.text;
+// if(value==null || value.isEmpty){
+//                     return '*Mandatory Field';
+//                   } 
+//   String value2=re_newPassword_controller.text;
+//   if(value2==null || value2.isEmpty){
+//                     return '*Mandatory Field';
+//                   }      
+//                   print(value + value2 + "");
+//                   if(!identical(value,value)){
+//                     return 'Re-Type New Password is incorrect';
+//                   }            
+//                 return null;
+                 
+// }
+// String? validate_email(){
+//  _formKey.currentState.validate();
+// }
 //Checking if user logout or not before leaving the app;
 void autoLogin() async{
   logindata = await SharedPreferences.getInstance();
@@ -66,6 +114,30 @@ void autoLogin() async{
   }
 
 
+ bool isFrontImage(double angle){
+    final degree90 = Math.pi/2;
+    final degree270 = 3*Math.pi/2;
+    return angle<=degree90 || angle>=degree270;
+  }
+Future flipImage() async{
+//  controller.addListener(() {
+  // if(controller.isCompleted){
+
+    showFront = !showFront;
+    if(!showFront){
+     controller.reverse();
+      
+  }
+  // if(controller.isDismissed){
+    else{
+ controller.forward();
+
+  }
+//  });             
+  
+    // await controller.reverse();
+    // controller.repeat();
+}
   //Setting visibility and contents of textfield for form2
   void setVisibility(){
     setState(() {
@@ -80,26 +152,62 @@ void autoLogin() async{
 void dispose(){
   email_controller.dispose();
   password_controller.dispose();
+  controller.dispose();
   super.dispose();
 }
 
 
 @override
 Widget build(BuildContext context){
+  
   return Scaffold(
+    resizeToAvoidBottomInset: false,
     appBar: AppBar(
       title: Text(
         "FoodGuru Login"
       ),
     ),
     body: Center(
-
+    
       //Form1
       child: Form(
          key: _formKey,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
+            children: [
+         AnimatedBuilder(
+            animation: controller,
+            builder: (context, child) {
+              double angle = controller.value * -Math.pi;
+              // if(showFront) angle += anglePlus; 
+        final transform = Matrix4.identity()
+        ..setEntry(3, 2, 0.001)
+        ..rotateY(angle);
+              return Transform(
+                transform: transform,
+                alignment: Alignment.center,
+                child: isFrontImage(angle.abs())?Transform(
+                  transform: transform,
+                  alignment: Alignment.center,              
+                  child: Container(
+                    height: 105.0,
+                    margin: EdgeInsets.only(top: 20),
+                    alignment: Alignment.center,
+                    child: cardFront,
+                  ),
+                ):Transform(
+                  transform: transform,
+                  alignment: Alignment.center,              
+                  child: Container(
+                    height: 105.0,
+                    margin: EdgeInsets.only(top: 20),
+                    alignment: Alignment.center,
+                    child: cardBack,
+                  ),
+                ),
+              );
+            },
+          ),                    
         Visibility(
           visible: isVisibile,
           child: Column(children: [
@@ -107,18 +215,21 @@ Widget build(BuildContext context){
           Text("Login Form",
           style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
           ),
-
+        
           //Email TextField
           Padding(padding: const EdgeInsets.all(15.0),
           child: SizedBox(
             width: 300.0,
             child: TextFormField(
+              // onChanged: (text) {
+              //     _formKey.currentState!.validate();
+              // },
               controller: email_controller,
               decoration: InputDecoration(
             border: OutlineInputBorder(),
             labelText: 'email',
               ),
-
+        
               //Email Validation
               validator: (value) {
                 if(value==null || value.isEmpty){
@@ -137,23 +248,27 @@ Widget build(BuildContext context){
             ),
           ),
           ),
-
+        
           //Password TextField For Form1
               Padding(padding: const EdgeInsets.all(15.0),
               child: SizedBox(
           width: 300.0,
           child: TextFormField(
+            // onChanged: (Text){
+            //   _formKey.currentState!.validate();
+        
+            // },
               obscureText: true,
               controller: password_controller,
               decoration: InputDecoration(
             border: OutlineInputBorder(),
             labelText: 'password',
               ),
-
+        
               // Password Validation
               validator: (value) {
                 print(logindata.getString('password'));
-
+        
                  if(value==null || value.isEmpty){
                   return '*Mandatory Field';
                 }           
@@ -165,25 +280,33 @@ Widget build(BuildContext context){
             ),
               ),
               ),
-
+        
               ElevatedButton(onPressed:(){ 
+                            //  flipImage();
+        FocusScope.of(context).unfocus();
+
           if(_formKey.currentState!.validate()){
             ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(content: Text('Logging In..')),
+              const SnackBar(backgroundColor: Colors.orange,
+               padding: EdgeInsets.all(15.0),
+                   behavior: SnackBarBehavior.floating,
+               duration: const Duration(milliseconds: 2000),content: Text('Logging In..')),
             );
-
+           
             //Setting user as LoggedIn Storage
             logindata.setBool('login', false);
                 print(logindata.getString('password'));
             
              //Navigating To Welcome Page
-            Navigator.push(context, MaterialPageRoute(builder: (context)=> WelcomePage()));
-          
+          Future.delayed(const Duration(seconds: 2), (){       
+                 Navigator.push(context, MaterialPageRoute(builder: (context)=> WelcomePage()));
+});
           }
               }, child: Text("Log-In"),
               ),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                     Text("Does Not Have Account?"),
               TextButton(onPressed: (){ setVisibility();}, child: Text('Forgot Password')),
@@ -192,7 +315,7 @@ Widget build(BuildContext context){
               ],    
               ),
         ),
-      Visibility(
+            Visibility(
         visible: !isVisibile,
         child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
@@ -201,7 +324,7 @@ Widget build(BuildContext context){
           Text("Forgot Password ",
           style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
           ),
-
+        
           //Email Verification For Form1 Reset Password
           Padding(padding: const EdgeInsets.all(15.0),
           child: SizedBox(
@@ -235,91 +358,94 @@ Widget build(BuildContext context){
             // ScaffoldMessenger.of(context).showSnackBar(
             //   const SnackBar(content: Text('A reset password mail has been sent')),
             // );
-
+        
             //Dialog Box For Updating Password
             showDialog(context: context, builder: (context){
-              return Visibility(
-                visible: !isVisibile,
-                child: Dialog(shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
-                elevation: 16,
-                child: Container(
-                  child:Column(children: [
-                          SizedBox(height: 50.0,),
-                        Text("Reset Password",
-                        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                        ),
-                        //New Password Field
-                        Padding(padding: const EdgeInsets.all(15.0),
-                        child: SizedBox(
+              return Form(
+                key: _resetKey,
+                child: Visibility(
+                  visible: !isVisibile,
+                  child: Dialog(shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+                  elevation: 16,
+                  child: Container(
+                    height: MediaQuery.of(context).size.height-400,
+                    child:Column(children: [
+                            SizedBox(height: 50.0,),
+                          Text("Reset Password",
+                          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                          ),
+                          //New Password Field
+                          Padding(padding: const EdgeInsets.all(15.0),
+                          child: SizedBox(
+                            width: 300.0,
+                            child: TextFormField(
+                   obscureText: true,
+                  controller: newPassword_controller,
+                  decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'New Password',
+                  ),
+                  validator: (value) {
+                    if(value==null || value.isEmpty){
+                      return '*Mandatory Field';
+                    }
+                  return  null;
+                  },
+                            ),
+                          ),
+                          ),
+                
+                          //Re-Enter New Password Field
+                  Padding(padding: const EdgeInsets.all(15.0),
+                  child: SizedBox(
                           width: 300.0,
                           child: TextFormField(
-                 obscureText: true,
-                controller: newPassword_controller,
-                decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'New Password',
-                ),
-                validator: (value) {
-                  if(value==null || value.isEmpty){
-                    return '*Mandatory Field';
-                  } 
-                 
-                return  null;
-                },
-                          ),
-                        ),
-                        ),
-
-                        //Re-Enter New Password Field
-                Padding(padding: const EdgeInsets.all(15.0),
-                child: SizedBox(
-                        width: 300.0,
-                        child: TextFormField(
-                obscureText: true,
-                controller: re_newPassword_controller,
-                decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Re-Type New Password',
-                ),
-                //Re-Enter New Password Field Validation
-                validator: (value) {
-                   if(value==null || value.isEmpty){
-                    return '*Mandatory Field';
-                  }      
-                  if(re_newPassword_controller.text!=newPassword_controller.text){
-                    return 'Re-Type New Password is incorrect';
-                  }            
-                return null;
-                },
-                          ),
-                ),
-                ),
-                ElevatedButton(onPressed:(){ 
-                        if(_formKey.currentState!.validate()){
-                         
-                        
-                        
-                        String new_password = newPassword_controller.text;
-                         Navigator.pop(context);
-                          ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(content: Text('Password Updated Successfully!!')),
-                          );
-                          print(logindata.getString('password'));
-
-                          //Updating Password
-                          setState(() {
-                           isVisibile=true;
-                           logindata.setString("password",new_password);
-                         });
-                          // Navigator.push(context, MaterialPageRoute(builder: (context)=> LoginPage()));
-                          
-                          
-                        }
-                }, child: Text("Update Password"),
-                ),
-                ],    
-                ),
-                ),
+                  obscureText: true,
+                  controller: re_newPassword_controller,
+                  decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Re-Type New Password',
+                  ),
+                  //Re-Enter New Password Field Validation
+                  validator: (value) {
+                    // errorText: _errorText_newpassword,
+                     if(value==null || value.isEmpty){
+                      return '*Mandatory Field';
+                    }      
+                    print(re_newPassword_controller.text +" " + newPassword_controller.text + "");
+                    if(re_newPassword_controller.text!=newPassword_controller.text){
+                      return 'Re-Type New Password is incorrect';
+                    }            
+                  return null;
+                  },
+                            ),
+                  ),
+                  ),
+                  ElevatedButton(onPressed:(){ 
+                          if(_resetKey.currentState!.validate()){
+                            // if(_errorText_newpassword==null){
+                          String new_password = newPassword_controller.text;
+                           Navigator.pop(context);
+                            ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('Password Updated Successfully!!')),
+                            );
+                            print(logindata.getString('password'));
+                
+                            //Updating Password
+                            setState(() {
+                             isVisibile=true;
+                             logindata.setString("password",new_password);
+                           });
+                            // Navigator.push(context, MaterialPageRoute(builder: (context)=> LoginPage()));
+                            
+                            
+                          }
+                  }, child: Text("Update Password"),
+                  ),
+                  ],    
+                  ),
+                  ),
+                  ),
                 ),
               ); 
               });
@@ -334,11 +460,13 @@ Widget build(BuildContext context){
         }, child: Text("Back"),)
         ],    
         ),
-      )
-      ]
-      ),
+            )
+            ]
+            ),
       ),
     ),
   ); 
+
+ 
 }
 }
---
+++ lib/welcomepage.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:foody_guru/main.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class HomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'FoodyGuru',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: WelcomePage(),
+    );
+  }
+}
+class WelcomePage extends StatefulWidget{
+  @override
+  _WelcomePageState createState() => _WelcomePageState();
+}
+class _WelcomePageState extends State<WelcomePage>{
+  late SharedPreferences logindata;
+  late String userId = '';
+  late Image bgImg ;
+  @override
+  void initState(){
+    super.initState();
+    initial_fun();
+    bgImg=Image.asset("Images/qoute.png");
+  }
+  void initial_fun() async{
+    logindata = await SharedPreferences.getInstance();
+    setState(() {
+      userId = logindata.getString('email').toString();
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Stack(
+      children:[ 
+         Padding(
+           padding: EdgeInsets.all(5.0),
+           child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("Images/quote.png"), // <-- BACKGROUND IMAGE
+                fit: BoxFit.fill,
+              ),
+            ),
+                 ),
+         ),
+        
+        Scaffold(
+                          backgroundColor: Colors.transparent, // <-- SCAFFOLD WITH TRANSPARENT BG
+
+        appBar: AppBar(
+          title: Text('Welcome To The DashBoard'),
+        ),
+        body: 
+            
+                 Column(
+                  //  mainAxisAlignment: MainAxisAlignment.start,
+                  //  crossAxisAlignment: CrossAxisAlignment.center,
+                   children: [
+                     SizedBox(height: 10.0,),
+                     Padding(
+                       padding: EdgeInsets.fromLTRB(0, 0,5.0,0),
+                       child: Container(
+                         alignment: Alignment.center,
+                         child: Column(
+                           children: [
+                              Align(
+                                alignment: Alignment.topRight,
+                                child: ElevatedButton(
+                                              onPressed: (){
+                                                logindata.setBool('login',true);
+                                              Navigator.pushReplacement(context, new MaterialPageRoute(builder: (context) => LoginPage()));
+                                            }, 
+                                            child: Text('LogOut'),
+                                            ),
+                              ),
+                             Text(
+                              'Welcome $userId',
+                              style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),
+                                     ),
+                                    
+                           ],
+                         ),
+                       ),
+                     ),
+                
+                   ],
+                 ),
+                 ),
+      ],
+        );      
+       
+  }
+}
